vl197602/hyper_tomato

Hyper: Add build tools

build.sh
@@ -1,0 +1,171 @@
+#!/bin/bash
+rm .version
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+cyan='\033[01;36m'
+blue='\033[01;34m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
+DEFCONFIG="cyanogenmod_tomato-64_defconfig"
+KERNEL="Image"
+
+#Hyper Kernel Details
+BASE_VER="Hyper"
+VER="-v1-$(date +"%Y-%m-%d"-%H%M)-"
+Hyper_VER="$BASE_VER$VER$TC"
+
+# Vars
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=Karthick111
+export KBUILD_BUILD_HOST=server1
+export LOCALVERSION="-Hyper™-v1"
+
+# Paths
+KERNEL_DIR=`pwd`
+RESOURCE_DIR="/home/android/kernel/tomato"
+ANYKERNEL_DIR="$RESOURCE_DIR/hyper"
+TOOLCHAIN_DIR="/home/android/kernel/tc"
+REPACK_DIR="$ANYKERNEL_DIR"
+PATCH_DIR="$ANYKERNEL_DIR/patch"
+MODULES_DIR="$ANYKERNEL_DIR/modules"
+ZIP_MOVE="$RESOURCE_DIR/kernel_out"
+ZIMAGE_DIR="$KERNEL_DIR/arch/arm64/boot"
+
+# Functions
+function make_kernel {
+		make $DEFCONFIG $THREAD
+		make $KERNEL $THREAD
+                make dtbs $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/zImage
+}
+
+function make_modules {
+		cd $KERNEL_DIR
+		make modules $THREAD
+		find $KERNEL_DIR -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+		cd $MODULES_DIR
+        $STRIP --strip-unneeded *.ko
+        cd $KERNEL_DIR
+}
+
+function make_dtb {
+		$KERNEL_DIR/dtbToolCM -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+		cp -vr $KERNEL_DIR/arch/arm64/boot/dt.img $REPACK_DIR/dtb
+}
+
+function make_zip {
+		cd $REPACK_DIR
+                zip -r `echo $Hyper_VER$TC`.zip *
+		mv  `echo $Hyper_VER$TC`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+DATE_START=$(date +"%s")
+
+
+echo -e "${green}"
+echo "--------------------------------------------------------"
+echo "Wellcome !!!   Initiatig To Compile $Hyper_VER    "
+echo "--------------------------------------------------------"
+echo -e "${restore}"
+
+echo -e "${cyan}"
+while read -p "Plese Select Desired Toolchain for compiling Hyper Kernel
+
+SABERMOD-4.9---->(1)
+
+UBERTC-4.9---->(2)
+
+
+" echoice
+do
+case "$echoice" in
+	1 )
+		export CROSS_COMPILE=$TOOLCHAIN_DIR/saber-4.9/bin/aarch64-
+		export LD_LIBRARY_PATH=$TOOLCHAIN_DIR/saber-4.9/lib/
+		STRIP=$TOOLCHAIN_DIR/saber-4.9/bin/aarch64-strip
+		TC="SM"
+		rm -rf $MODULES_DIR/*
+		rm -rf $ZIP_MOVE/*
+		rm -rf $KERNEL_DIR/arch/arm/boot/dt.img
+		cd $ANYKERNEL_DIR
+		rm -rf zImage
+		rm -rf dtb
+                cd $KERNEL_DIR
+		make clean && make mrproper
+		echo "cleaned directory"
+		echo "Compiling Hyper Kernel Using SABERMOD-4.9 Toolchain"
+		break
+		;;
+	2 )
+		export CROSS_COMPILE=$TOOLCHAIN_DIR/uber-4.9/bin/aarch64-linux-android-
+		export LD_LIBRARY_PATH=$TOOLCHAIN_DIR/uber-4.9/lib/
+		STRIP=$TOOLCHAIN_DIR/uber-4.9/bin/aarch64-linux-android-strip
+		TC="UB"
+		rm -rf $MODULES_DIR/*
+		rm -rf $ZIP_MOVE/*
+		rm -rf $KERNEL_DIR/arch/arm/boot/dt.img
+		cd $ANYKERNEL_DIR
+		rm -rf zImage
+		rm -rf dtb
+		cd $KERNEL_DIR
+		make clean && make mrproper
+		echo "cleaned directory"
+		echo "Compiling Hyper Kernel Using UBERTC-4.9 Toolchain"
+		break
+		;;
+
+	* )
+		echo
+		echo "Invalid Selection try again !!"
+		echo
+		;;
+esac
+done
+echo -e "${restore}"
+
+echo
+while read -p "Do you want to start Building Hyper Kernel ?
+
+Yes Or No ? 
+
+Enter Y for Yes Or N for No
+" dchoice
+do
+case "$dchoice" in
+	y|Y )
+		make_kernel
+		make_dtb
+		make_modules
+		make_zip
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid Selection try again !!"
+		echo
+		;;
+esac
+done
+echo -e "${green}"
+echo $Hyper_VER$TC.zip
+echo "------------------------------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo " "
+cd $ZIP_MOVE
+ls
+ftp uploads.androidfilehost.com

hyper/anykernel.sh
@@ -1,0 +1,170 @@
+# AnyKernel 2.0 Ramdisk Mod Script 
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+do.devicecheck=1
+do.system=0
+do.initd=0
+do.modules=1
+do.cleanup=1
+device.name1=YUREKA
+device.name2=YU AO5510
+device.name3=
+device.name4=
+device.name5=
+
+# shell variables
+block=/dev/block/platform/7824900.sdhci/by-name/boot;
+
+## end setup
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+patch=/tmp/anykernel/patch;
+
+chmod -R 755 $bin;
+mkdir -p $ramdisk $split_img;
+cd $ramdisk;
+
+OUTFD=`ps | grep -v "grep" | grep -oE "update(.*)" | cut -d" " -f3`;
+ui_print() { echo "ui_print $1" >&$OUTFD; echo "ui_print" >&$OUTFD; }
+
+# dump boot and extract ramdisk
+dump_boot() {
+  dd if=$block of=/tmp/anykernel/boot.img;
+  $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Dumping/unpacking image failed. Aborting...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
+  fi;
+  gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+  cd $split_img;
+  cmdline=`cat *-cmdline`;
+  board=`cat *-board`;
+  base=`cat *-base`;
+  pagesize=`cat *-pagesize`;
+  kerneloff=`cat *-kerneloff`;
+  ramdiskoff=`cat *-ramdiskoff`;
+  tagsoff=`cat *-tagsoff`;
+  if [ -f *-second ]; then
+    second=`ls *-second`;
+    second="--second $split_img/$second";
+    secondoff=`cat *-secondoff`;
+    secondoff="--second_offset $secondoff";
+  fi;
+  if [ -f /tmp/anykernel/zImage ]; then
+    kernel=/tmp/anykernel/zImage;
+  else
+    kernel=`ls *-zImage`;
+    kernel=$split_img/$kernel;
+  fi;
+  if [ -f /tmp/anykernel/dtb ]; then
+    dtb="--dt /tmp/anykernel/dtb";
+  elif [ -f *-dtb ]; then
+    dtb=`ls *-dtb`;
+    dtb="--dt $split_img/$dtb";
+  fi;
+  cd $ramdisk;
+  find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb --output /tmp/anykernel/boot-new.img;
+  if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "Repacking Boot.img failed. Aborting operation...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
+  fi;
+  dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+# backup_file <file>
+backup_file() { cp $1 $1~; }
+
+# replace_string <file> <if search string> <original string> <replacement string>
+replace_string() {
+  if [ -z "$(grep "$2" $1)" ]; then
+      sed -i "s;${3};${4};" $1;
+  fi;
+}
+
+# insert_line <file> <if search string> <before/after> <line match string> <inserted line>
+insert_line() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;${5};" $1;
+  fi;
+}
+
+# replace_line <file> <line replace string> <replacement line>
+replace_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}s;.*;${3};" $1;
+  fi;
+}
+
+# remove_line <file> <line match string>
+remove_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}d" $1;
+  fi;
+}
+
+# prepend_file <file> <if search string> <patch file>
+prepend_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo "$(cat $patch/$3 $1)" > $1;
+  fi;
+}
+
+# append_file <file> <if search string> <patch file>
+append_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo -ne "\n" >> $1;
+    cat $patch/$3 >> $1;
+    echo -ne "\n" >> $1;
+  fi;
+}
+
+# replace_file <file> <permissions> <patch file>
+replace_file() {
+  cp -fp $patch/$3 $1;
+  chmod $2 $1;
+}
+
+## end methods
+
+## AnyKernel permissions
+# set permissions for included files
+chmod -R 755 $ramdisk
+chmod 750 $ramdisk/sbin/busybox;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# insert initd scripts
+
+# insert patches
+
+# adb secure
+replace_string default.prop "ro.adb.secure=0" "ro.adb.secure=1" "ro.adb.secure=0";
+replace_string default.prop "ro.secure=0" "ro.secure=1" "ro.secure=0";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+

chaubeyprateek/android_kernel_xiaomi_hydrogen

build.sh
@@ -1,0 +1,171 @@
+#!/bin/bash
+rm .version
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+cyan='\033[01;36m'
+blue='\033[01;34m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
+DEFCONFIG="hydrogen_user_defconfig"
+KERNEL="Image"
+
+#Hyper Kernel Details
+BASE_VER="Hyper"
+VER="-v1-$(date +"%Y-%m-%d"-%H%M)-"
+Hyper_VER="$BASE_VER$VER$TC"
+
+# Vars
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=Karthick111
+export KBUILD_BUILD_HOST=server1
+export LOCALVERSION="-Hyper™-v1"
+
+# Paths
+KERNEL_DIR=`pwd`
+RESOURCE_DIR="/home/android/kernel/hydrogen"
+ANYKERNEL_DIR="$RESOURCE_DIR/hyper"
+TOOLCHAIN_DIR="/home/android/kernel/tc"
+REPACK_DIR="$ANYKERNEL_DIR"
+PATCH_DIR="$ANYKERNEL_DIR/patch"
+MODULES_DIR="$ANYKERNEL_DIR/modules"
+ZIP_MOVE="$RESOURCE_DIR/kernel_out"
+ZIMAGE_DIR="$KERNEL_DIR/arch/arm64/boot"
+
+# Functions
+function make_kernel {
+		make $DEFCONFIG $THREAD
+		make $KERNEL $THREAD
+                make dtbs $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/zImage
+}
+
+function make_modules {
+		cd $KERNEL_DIR
+		make modules $THREAD
+		find $KERNEL_DIR -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+		cd $MODULES_DIR
+        $STRIP --strip-unneeded *.ko
+        cd $KERNEL_DIR
+}
+
+function make_dtb {
+		$KERNEL_DIR/dtbToolCM -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+		cp -vr $KERNEL_DIR/arch/arm64/boot/dt.img $REPACK_DIR/dtb
+}
+
+function make_zip {
+		cd $REPACK_DIR
+                zip -r `echo $Hyper_VER$TC`.zip *
+		mv  `echo $Hyper_VER$TC`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+DATE_START=$(date +"%s")
+
+
+echo -e "${green}"
+echo "--------------------------------------------------------"
+echo "Wellcome !!!   Initiatig To Compile $Hyper_VER    "
+echo "--------------------------------------------------------"
+echo -e "${restore}"
+
+echo -e "${cyan}"
+while read -p "Plese Select Desired Toolchain for compiling Hyper Kernel
+
+SABERMOD-4.9---->(1)
+
+UBERTC-4.9---->(2)
+
+
+" echoice
+do
+case "$echoice" in
+	1 )
+		export CROSS_COMPILE=$TOOLCHAIN_DIR/saber-4.9/bin/aarch64-
+		export LD_LIBRARY_PATH=$TOOLCHAIN_DIR/saber-4.9/lib/
+		STRIP=$TOOLCHAIN_DIR/saber-4.9/bin/aarch64-strip
+		TC="SM"
+		rm -rf $MODULES_DIR/*
+		rm -rf $ZIP_MOVE/*
+		rm -rf $KERNEL_DIR/arch/arm/boot/dt.img
+		cd $ANYKERNEL_DIR
+		rm -rf zImage
+		rm -rf dtb
+                cd $KERNEL_DIR
+		make clean && make mrproper
+		echo "cleaned directory"
+		echo "Compiling Hyper Kernel Using SABERMOD-4.9 Toolchain"
+		break
+		;;
+	2 )
+		export CROSS_COMPILE=$TOOLCHAIN_DIR/uber-4.9/bin/aarch64-linux-android-
+		export LD_LIBRARY_PATH=$TOOLCHAIN_DIR/uber-4.9/lib/
+		STRIP=$TOOLCHAIN_DIR/uber-4.9/bin/aarch64-linux-android-strip
+		TC="UB"
+		rm -rf $MODULES_DIR/*
+		rm -rf $ZIP_MOVE/*
+		rm -rf $KERNEL_DIR/arch/arm/boot/dt.img
+		cd $ANYKERNEL_DIR
+		rm -rf zImage
+		rm -rf dtb
+		cd $KERNEL_DIR
+		make clean && make mrproper
+		echo "cleaned directory"
+		echo "Compiling Hyper Kernel Using UBERTC-4.9 Toolchain"
+		break
+		;;
+
+	* )
+		echo
+		echo "Invalid Selection try again !!"
+		echo
+		;;
+esac
+done
+echo -e "${restore}"
+
+echo
+while read -p "Do you want to start Building Hyper Kernel ?
+
+Yes Or No ? 
+
+Enter Y for Yes Or N for No
+" dchoice
+do
+case "$dchoice" in
+	y|Y )
+		make_kernel
+		make_dtb
+		make_modules
+		make_zip
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid Selection try again !!"
+		echo
+		;;
+esac
+done
+echo -e "${green}"
+echo $Hyper_VER$TC.zip
+echo "------------------------------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo " "
+cd $ZIP_MOVE
+ls
+ftp uploads.androidfilehost.com

hyper/anykernel.sh
@@ -1,0 +1,170 @@
+# AnyKernel 2.0 Ramdisk Mod Script 
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+do.devicecheck=1
+do.system=0
+do.initd=0
+do.modules=1
+do.cleanup=1
+device.name1=hydrogen
+device.name2=helium
+device.name3=
+device.name4=
+device.name5=
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+
+## end setup
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+patch=/tmp/anykernel/patch;
+
+chmod -R 755 $bin;
+mkdir -p $ramdisk $split_img;
+cd $ramdisk;
+
+OUTFD=`ps | grep -v "grep" | grep -oE "update(.*)" | cut -d" " -f3`;
+ui_print() { echo "ui_print $1" >&$OUTFD; echo "ui_print" >&$OUTFD; }
+
+# dump boot and extract ramdisk
+dump_boot() {
+  dd if=$block of=/tmp/anykernel/boot.img;
+  $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Dumping/unpacking image failed. Aborting...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
+  fi;
+  gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+  cd $split_img;
+  cmdline=`cat *-cmdline`;
+  board=`cat *-board`;
+  base=`cat *-base`;
+  pagesize=`cat *-pagesize`;
+  kerneloff=`cat *-kerneloff`;
+  ramdiskoff=`cat *-ramdiskoff`;
+  tagsoff=`cat *-tagsoff`;
+  if [ -f *-second ]; then
+    second=`ls *-second`;
+    second="--second $split_img/$second";
+    secondoff=`cat *-secondoff`;
+    secondoff="--second_offset $secondoff";
+  fi;
+  if [ -f /tmp/anykernel/zImage ]; then
+    kernel=/tmp/anykernel/zImage;
+  else
+    kernel=`ls *-zImage`;
+    kernel=$split_img/$kernel;
+  fi;
+  if [ -f /tmp/anykernel/dtb ]; then
+    dtb="--dt /tmp/anykernel/dtb";
+  elif [ -f *-dtb ]; then
+    dtb=`ls *-dtb`;
+    dtb="--dt $split_img/$dtb";
+  fi;
+  cd $ramdisk;
+  find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb --output /tmp/anykernel/boot-new.img;
+  if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "Repacking Boot.img failed. Aborting operation...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
+  fi;
+  dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+# backup_file <file>
+backup_file() { cp $1 $1~; }
+
+# replace_string <file> <if search string> <original string> <replacement string>
+replace_string() {
+  if [ -z "$(grep "$2" $1)" ]; then
+      sed -i "s;${3};${4};" $1;
+  fi;
+}
+
+# insert_line <file> <if search string> <before/after> <line match string> <inserted line>
+insert_line() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;${5};" $1;
+  fi;
+}
+
+# replace_line <file> <line replace string> <replacement line>
+replace_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}s;.*;${3};" $1;
+  fi;
+}
+
+# remove_line <file> <line match string>
+remove_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}d" $1;
+  fi;
+}
+
+# prepend_file <file> <if search string> <patch file>
+prepend_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo "$(cat $patch/$3 $1)" > $1;
+  fi;
+}
+
+# append_file <file> <if search string> <patch file>
+append_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo -ne "\n" >> $1;
+    cat $patch/$3 >> $1;
+    echo -ne "\n" >> $1;
+  fi;
+}
+
+# replace_file <file> <permissions> <patch file>
+replace_file() {
+  cp -fp $patch/$3 $1;
+  chmod $2 $1;
+}
+
+## end methods
+
+## AnyKernel permissions
+# set permissions for included files
+chmod -R 755 $ramdisk
+chmod 750 $ramdisk/sbin/busybox;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# insert initd scripts
+
+# insert patches
+
+# adb secure
+replace_string default.prop "ro.adb.secure=0" "ro.adb.secure=1" "ro.adb.secure=0";
+replace_string default.prop "ro.secure=0" "ro.secure=1" "ro.secure=0";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+